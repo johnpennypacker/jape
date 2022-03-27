2bd34fa6cdbe3caafd5f9a702c17d8933acb2e14
John Pennypacker <jpennypacker@uri.edu>
2022-03-27 17:18:32 -0400

    updates README

--- inc/header-functions.php
+++ inc/header-functions.php
@@ -1,5 +1,22 @@
 <?php
 /**
+ * jape theme header bits
+ *
+ * @package jape
+ */
+
+/**
+ * Adds Google Tag Manager code to <head>
+ */
+function jape_gtm() {
+	$gtm = '';
+	echo "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','" . $gtm . "');</script>";
+}
+// add_action( 'wp_head', 'jape_gtm' );
+
+
+
+/**
  * Add open graph elements to the <head>
  *
  * @todo: allow other twitter handles
@@ -67,16 +84,3 @@ function jape_open_graph() {
 if ( ! defined( 'WPSEO_VERSION' ) ) {
 	add_action( 'wp_head', 'jape_open_graph' );
 }
-
-
-
-/**
- * Adds Google Tag Manager code to <head>
- */
-function jape_gtm() {
-	$gtm = 'GTM-WMPL9S';
-	echo "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','" . $gtm . "');</script>";
-}
-// add_action( 'wp_head', 'jape_gtm' );
-
-
