1d340f3a5e8abfc77d646714c476c8c9acce6082
John Pennypacker <jpennypacker@uri.edu>
2022-04-14 19:54:01 -0400

    consolidates page and single to index.php

--- index.php
+++ index.php
@@ -40,6 +40,20 @@ dynamic_sidebar( 'precontent' );
 				 */
 				get_template_part( 'template-parts/content', get_post_type() );
 
+				if ( 'post' === get_post_type() ) {
+					the_post_navigation(
+						array(
+							'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'jape' ) . '</span> <span class="nav-title">%title</span>',
+							'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'jape' ) . '</span> <span class="nav-title">%title</span>',
+						)
+					);
+				}
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
 			endwhile;
 
 			the_posts_navigation();
