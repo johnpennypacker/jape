766866892b4331d2d432982e2c4865ac13ba938b
John Pennypacker <jpennypacker@uri.edu>
2022-06-06 12:02:32 -0400

    style updates, theme.json error resolved

--- functions.php
+++ functions.php
@@ -65,8 +65,7 @@ function jape_setup() {
 	
 	add_theme_support('editor-styles');
 	add_editor_style( 'style-editor.css' );
-
-
+	
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
